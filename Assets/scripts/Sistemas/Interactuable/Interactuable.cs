d7ab1717877c5a24d34ab916fe2493dd3faf083b
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-04 11:27:53 -0400

    Interactuable básico

---
+++ Assets/scripts/Sistemas/Interactuable/Interactuable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.Events;
+
+public class Interactuable : MonoBehaviour
+{
+   [SerializeField]
+   private UnityEvent interactuar;
+
+   public void Interactuar()
+   {
+      interactuar?.Invoke();
+   }
+
+   private void Update()
+   {
+      if(Input.GetKeyDown(KeyCode.A))
+         Interactuar();
+   }
+}
