e716f5fbda0532b0fc016de881abc55035276f48
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-04 11:27:13 -0400

    Dialogo

---
+++ Assets/scripts/Sistemas/Dialogo/Dialogo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using static DialogManager;
+
+public class Dialogo : MonoBehaviour
+{
+   [TextArea]
+   public string[] dialogos;
+
+   [HideInInspector] public bool iniciado;
+   
+   public void Iniciar()
+   {
+      if (!iniciado)
+         instance.StartDialogue(this);
+      else
+         instance.NextLine();
+   }
+   public string this[int i] => dialogos[i];
+}
