6ab74916bea3dad2f9073e500fb014280a84a918
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-11 17:38:20 -0400

    meta assets

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+   public CinemachineTargetGroup TargetGroup;
+
+
+   private CinemachineTargetGroup.Target target;
+   [SerializeField] private float targetWeight;
+   
+   public void SetTarget(MonoBehaviour target)
+   {
+      
+      this.target.target = target.transform;
+      this.target.weight = targetWeight;
+      TargetGroup.m_Targets[1] = this.target;
+   }
+
+   public void Reset()
+   {
+      target.target = null;
+      target.weight = 0;
+      TargetGroup.m_Targets[1] = target;
+   }
+}
