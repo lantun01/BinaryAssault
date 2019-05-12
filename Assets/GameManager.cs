89bea99f54c4b0761b02065db0ce7efca5340385
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-12 12:02:20 -0400

    chips & gitignore

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        Application.targetFrameRate = -1;
+        QualitySettings.vSyncCount = 0;
+
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
