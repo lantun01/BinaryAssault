882a7efb1bec1db113db433ba454fa855955cf7f
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-14 10:51:26 -0400

    Crea evento onMorir

--- Assets/scripts/Gameplay/Character.cs
+++ Assets/scripts/Gameplay/Character.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Character : MonoBehaviour
 {
@@ -12,11 +13,13 @@ public class Character : MonoBehaviour
     public Rigidbody2D rb;
     public Vector2 mirada;
     public Vector3 dirMov;
+    public UnityEvent OnMorir;
     protected int saludActual { get=>_saludActual; set
         {
             if (value<0)
             {
                 _saludActual = 0;
+                OnMorir?.Invoke();
             }
             else if (value>saludMaxima)
             {
