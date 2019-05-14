fec38309af5f51630ea2d9e268a2330d7114f573
JoseMiguelPizarro <37783633+JoseMiguelPizarro@users.noreply.github.com>
2019-05-14 11:17:51 -0400

    Armadrop ahora requiere spriterenderer y boxcollider2d

--- Assets/scripts/Gameplay/ArmaDrop.cs
+++ Assets/scripts/Gameplay/ArmaDrop.cs
@@ -3,9 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(SpriteRenderer),typeof(BoxCollider2D))]
 public class ArmaDrop : MonoBehaviour
 {
     public ArmaData data;
+
+    private void OnValidate()
+    {
+        Collider2D coll = GetComponent<BoxCollider2D>();
+        coll.isTrigger = true;
+    }
 
     public void RecogerArma(Player player)
     {
